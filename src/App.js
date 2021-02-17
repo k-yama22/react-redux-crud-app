4752a699a06c91118b4110828e7c85d67832904c
k-yama22 <ymgt.katsuya.0322@gmail.com>
2021-02-17 15:41:51 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import REACT from "react";
 
 const App = () => {
+  const dom = "Hi!"
+  const onClickText = () => {
+    console.log(dom);
+  }
   return (
+    <>
     <h1>Hello World</h1>
+    <div>{dom}</div>
+    <input type="text" onClick={onClickText} />
+    </>
   );
 }
 
