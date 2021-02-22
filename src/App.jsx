be8fa6506466072060c3766e8b9a80d5c74d2389
k-yama22 <ymgt.katsuya.0322@gmail.com>
2021-02-22 10:37:51 +0900

    props,prop-types,Stateについてお試し実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import React from "react";
-import PropTypes from "prop-types"
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
 const TodosList = (props) => {
   const { profiles } = props;
-  console.log(props)
+  console.log(props);
   return (
     <div>
       <ul>
-        {profiles.map((profile) => {
-          return <div key={profile}><li>{profile}</li></div>;
+        {profiles.map((prop, index) => {
+          return (
+            <li key={index}>
+              {prop.name}と申します。{prop.age}歳です
+            </li>
+          );
         })}
       </ul>
     </div>
@@ -16,19 +20,56 @@ const TodosList = (props) => {
 };
 
 const App = () => {
-  const profiles = ["山口", 24 ];
-  const dom = "Hi!";
-  const onClickText = () => {
-    console.log(dom);
+  const onClickPlus = () => {
+    setNum(num + 1);
+  };
+  const onClickMinus = () => {
+    setNum(num - 1);
+  };
+
+  const [text, setText] = useState("");
+  const [num, setNum] = useState(0);
+  const [labelText, setLabelText] = useState("初期表示");
+  const profiles = [
+    { name: "山口", age: 24 },
+    { name: "田中", age: 26 },
+    { name: 2, age: 29 },
+  ];
+
+  const dom = `Hi,${num}`;
+  const onChangeText = (event) => {
+    setText(event.target.value);
+  };
+  const onclickDecide = () => {
+    setLabelText(text);
   };
   return (
     <>
+      {/* <div>
+        {profiles.map((profile) => {
+          return <TodosList name={profile.name} age={profile.age} />;
+        })}
+      </div> */}
       <TodosList profiles={profiles} />
       <h1>Hello World</h1>
       <div>{dom}</div>
-      <input type="text" onClick={onClickText} />
+      <input type="text" onChange={onChangeText} value={text} />
+      <button onClick={onClickPlus}>+1</button>
+      <button onClick={onClickMinus}>-1</button>
+      <button id="decide" onClick={onclickDecide}>
+        決定
+      </button>
+      <label htmlFor="decide">{labelText}</label>
     </>
   );
+};
+TodosList.propTypes = {
+  profiles: PropTypes.arrayOf(
+    PropTypes.exact({
+      name: PropTypes.string,
+      age: PropTypes.number,
+    })
+  ),
 };
 
 export default App;
